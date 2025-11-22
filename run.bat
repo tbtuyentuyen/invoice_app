8afed7404deaab4574e2a8d639997d83e5638d7b
imntp <tanphat3198@gmail.com>
2025-11-22 14:49:21 +0700

    Add: run.bat file

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+set INVOICE_APP_PATH=
+set CONFIG_PATH=%INVOICE_APP_PATH%/config.json
+set SHOP_INFO_PATH=%INVOICE_APP_PATH%/data/shop_info.json
+set BUYER_INFO_PATH=%INVOICE_APP_PATH%/data/buyer_info.json
+python %INVOICE_APP_PATH%/main.py
